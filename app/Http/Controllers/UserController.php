aeebc5009a016a2b7548ebe5f0a0f600c414d8ea
kazi <hasan.idb7@gmail.com>
2023-11-10 08:54:03 +0600

    Myfirst laravel Project

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function welcome(){
+        return "<h1>Hello, Laravel! </h1> ";
+    }
+
+}
