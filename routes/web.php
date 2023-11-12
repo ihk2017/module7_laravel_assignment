460ad01b99f132cbe781efe09cb99e63fecd3b9f
kazi <myemail@example.com>
2023-11-12 11:17:05 +0600

    Myfirst laravel Project

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 
-Route::get('/helloostad',[UserController::class,'welcome']);
+Route::get('/greeting',[UserController::class,'welcome']);
